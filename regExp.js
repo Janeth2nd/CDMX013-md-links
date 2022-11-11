f2769a64bb3fb142ff8cebd27b0e6f2c0660700a
Jane2do <107877817+Jane2do@users.noreply.github.com>
2022-11-10 19:30:34 -0600

    fetch is working0

--- regExp.js
+++ regExp.js
@@ -2,11 +2,10 @@ const fs = require('fs');
 const path = require('path')
 
 const pathText = path.join(__dirname, './readme.md');
-const readMe = fs.readFileSync(pathText, 'utf-8');
-//console.log(readMe);
+const readingFile = fs.readFileSync(pathText, 'utf-8');
 
 const regex = /\[(.+)\]\((https?:.+)\)/ig;
-const linksArray = Array.from(readMe.match(regex))
+const linksArray = Array.from(readingFile.match(regex))
 console.log(linksArray);
 
 
